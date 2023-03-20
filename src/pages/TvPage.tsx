dfd356666c7cfcc3be53bdce6a695135220f69fe
seoa <tjdkzz97@naver.com>
2023-03-21 00:11:36 +0900

    create tv program page

--- src/pages/TvPage.tsx
+++ src/pages/TvPage.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import LatestTvSection from "../features/tv/latest";
 
 const TvPage: React.FC = () => {
     return (
-        <div>Tv Page</div>
+        <div>
+            <LatestTvSection />
+        </div>
     )
 }
 
